a182983c4737f305d624f8212784a5425a3222c9
SamPaskewitz <55294273+SamPaskewitz@users.noreply.github.com>
2026-02-18 13:18:45 -0500

    revised contrasts to divide by sqrt(2)
    Thus the prior is for pair-wise mean differences.

--- R/contr_banova.R
+++ R/contr_banova.R
@@ -4,6 +4,8 @@
 #' @details Special care needs to be taken with Bayesian ANOVA to make sure that all factor level means (and pairwise mean differences) have equal prior distributions. R's default coding scheme (treatment coding) does not accomplish this: priors will not be equal. This function implements a normalized forward Helmert contrast, which will produce equal priors across factor levels as described in the paper below (note that they don't explicitly say that they're describing normalized forward Helmert contrasts in that paper, but that is what they're doing).
 #' Rouder, J. N., Morey, R. D., Speckman, P. L., & Province, J. M. (2012). Default Bayes factors for ANOVA designs. Journal of Mathematical Psychology, 56(5), 356–374. https://doi.org/10.1016/j.jmp.2012.08.001
 #'
+#' Note that the contrasts have been adapted to facilitate putting the prior on pair-wise mean differences, as in bayestestR::contr.equalprior_pairs.
+#'
 contr_banova = function(a){
   contr = matrix(0.0, nrow = a, ncol = a - 1)
   for(cl in 1:(a-1)){
@@ -14,5 +16,7 @@ contr_banova = function(a){
     # normalize
     contr[,cl] = contr[,cl]/sqrt(sum(contr[,cl]^2))
   }
+  # divide by sqrt(2) as in bayestestR::contr.equalprior_pairs
+  contr = contr/sqrt(2)
   return(contr)
 }
--- tests/testthat/test-contr_banova.R
+++ tests/testthat/test-contr_banova.R
@@ -1,19 +1,23 @@
 test_that("works for a = 2", {
   # from section 7.1 of Rouder et al. (2012)
-  expect_equal(contr_banova(2),
-               matrix(c(sqrt(2)/2, -sqrt(2)/2), nrow = 2, ncol = 1),
+  # note that contr_banova now divides by sqrt(2) as in bayestestR::contr.equalprior_pairs
+  target = matrix(c(sqrt(2)/2, -sqrt(2)/2), nrow = 2, ncol = 1)
+  expect_equal(contr_banova(2)*sqrt(2),
+               target,
                tolerance = 1e-2)
 })
 
 test_that("works for a = 5", {
+  target = matrix(c(0.89, -0.22, -0.22, -0.22, -0.22,
+                    0, 0.87, -0.29, -0.29, -0.29,
+                    0, 0, 0.82, -0.41, -0.41,
+                    0, 0, 0, 0.71, -0.71),
+                  nrow = 4,
+                  ncol = 5,
+                  byrow = TRUE) |> t()
   # from section 7.1 of Rouder et al. (2012)
-  expect_equal(contr_banova(5),
-               matrix(c(0.89, -0.22, -0.22, -0.22, -0.22,
-                        0, 0.87, -0.29, -0.29, -0.29,
-                        0, 0, 0.82, -0.41, -0.41,
-                        0, 0, 0, 0.71, -0.71),
-                      nrow = 4,
-                      ncol = 5,
-                      byrow = TRUE) %>% t(),
+  # note that contr_banova now divides by sqrt(2) as in bayestestR::contr.equalprior_pairs
+  expect_equal(contr_banova(5)*sqrt(2),
+               target,
                tolerance = 1e-2)
 })
